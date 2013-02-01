35ac3348d9697822ae78b1ff7efceb3622be828c
Andrew Cole <andrew.cole@illallangi.com>
2013-02-02 02:27:18 +1100

    Moved namespace

--- Illallangi.PaginatedList/PaginatedList.cs
+++ Illallangi.PaginatedList/PaginatedList.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Illallangi.PaginatedList
+namespace Illallangi
 {
     public class PaginatedList<T> : List<T>
     {
