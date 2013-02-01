0835d99b438521159a13f8ff898491278909e737
Andrew Cole <andrew.cole@illallangi.com>
2013-02-02 02:21:42 +1100

    Initial Commit

---
+++ Illallangi.PaginatedList/PaginatedList.cs
@@ -0,0 +1,113 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PaginatedList.cs" company="Illallangi Enterprises">
+// Copyright (C) 2013 Illallangi Enterprises
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Illallangi.PaginatedList
+{
+    public class PaginatedList<T> : List<T>
+    {
+        #region Fields
+
+        private readonly int currentCurrentPage;
+        private readonly int currentPageSize;
+        private readonly int currentTotalCount;
+        private readonly int currentFinalPage;
+
+        #endregion
+
+        #region Constructors
+
+        public PaginatedList(IQueryable<T> source, int? pageIndex, int pageSize = 10)
+        {
+            this.currentPageSize = pageSize;
+            this.currentTotalCount = source.Count();
+            this.currentFinalPage = (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+            var page = pageIndex ?? 1;
+
+            if (page < 1)
+            {
+                page = 1;
+            }
+
+            if (page > this.FinalPage)
+            {
+                page = this.FinalPage;
+            }
+
+            this.currentCurrentPage = page;
+
+            this.AddRange(source.Skip((this.CurrentPage - 1) * this.PageSize).Take(this.PageSize));
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int FirstPage
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public int PreviousPage
+        {
+            get
+            {
+                return this.CurrentPage > 1 ? this.CurrentPage - 1 : 1;
+            }
+        }
+
+        public int CurrentPage
+        {
+            get
+            {
+                return this.currentCurrentPage;
+            }
+        }
+
+        public int NextPage
+        {
+            get
+            {
+                return this.CurrentPage + 1 < this.FinalPage ? this.CurrentPage + 1 : this.FinalPage;
+            }
+        }
+
+        public int FinalPage
+        {
+            get
+            {
+                return this.currentFinalPage;
+            }
+        }
+
+        public int PageSize
+        {
+            get
+            {
+                return this.currentPageSize;
+            }
+        }
+
+        public int TotalCount
+        {
+            get
+            {
+                return this.currentTotalCount;
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ Illallangi.PaginatedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Illallangi Enterprises">
+// Copyright (C) 2013 Illallangi Enterprises
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Paginated List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Illallangi Enterprises")]
+[assembly: AssemblyProduct("Paginated List")]
+[assembly: AssemblyCopyright("Copyright © Illallangi Enterprises 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e80bdac-06f5-4ddd-b983-2b80e5eff52f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
